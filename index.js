dd042f8674d2a142aa6b2812d861a6e618e5aac1
mgkOV <myagkov.mww@gmail.com>
2018-05-30 17:34:37 +0300

    add google auth

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+const app = express();
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/calback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token:', accessToken);
+      console.log('refresh token:', refreshToken);
+      console.log('profile:', profile);
+      done(profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/calback', passport.authenticate('google'), (req, res) => {
+  // Successful authentication, redirect home.
+  res.redirect('/');
+});
 
 app.get('/', (req, res) => {
-  res.send({hi: 'there'})
-})
+  res.send({ hi: 'there' });
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server run on ${PORT}`)
-})
+  console.log(`Server run on ${PORT}`);
+});
