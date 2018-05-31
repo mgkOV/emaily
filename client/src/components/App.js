037f251be123d78427d17228a4cf91e84ec5a423
mgkOV <myagkov.mww@gmail.com>
2018-06-01 00:56:43 +0300

    add header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const App = () => {
-  return <div>Hi there!</div>;
-};
+import Header from './Header';
+import { fetchUser } from '../actions';
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
-export default App;
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Router>
+          <div>
+            <Header />
+            <Route path="/" exact component={Landing} />
+            <Route path="/surveys" exact component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default connect(null, { fetchUser })(App);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,10 @@
+import { FETCH_USER } from '../actions/types';
+
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_USER:
+      console.log(action.payload);
+      return state;
     default:
       return state;
   }
