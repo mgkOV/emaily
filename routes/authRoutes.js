881fa65fbc007f8fdae00deeef095714704a493d
mgkOV <myagkov.mww@gmail.com>
2018-05-31 15:53:05 +0300

    add cookie sessions

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,9 +8,20 @@ module.exports = app => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/');
+    }
+  );
 
   app.get('/api/current_user', (req, res) => {
-    res.send(req.user || { hi: 'there' });
+    res.send(req.user || { err: 'no user' });
+  });
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
   });
 };
