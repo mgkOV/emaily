7f10210683fe61ab02f21a3ed5ea91464a983422
mgkOV <myagkov.mww@gmail.com>
2018-06-01 16:24:57 +0300

    add payment component

---
+++ client/src/components/Payments.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payment extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Payment;
