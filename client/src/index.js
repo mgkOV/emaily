ad8bfbde56ceba70f0ce558d10d17aeba0bdf4df
mgkOV <myagkov.mww@gmail.com>
2018-06-04 16:09:52 +0300

    add mailer

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,10 @@ import 'materialize-css/dist/css/materialize.min.css';
 import reducers from './reducers';
 import App from './components/App';
 
+// For test
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
